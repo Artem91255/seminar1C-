a4e81ec6f376b0ac5fabf79646707601ee181a18
artem.chernomazov <pechenkazz@mail.ru>
2022-08-22 22:50:18 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if(x*x==y)
+{
+    System.Console.WriteLine("Da");
+}
+else
+{
+    System.Console.WriteLine("net");
+}
