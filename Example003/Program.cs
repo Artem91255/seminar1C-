f7fbd7ee4dd80dc39111c17c4da0460c95570bfc
artem.chernomazov <pechenkazz@mail.ru>
2022-08-22 22:50:44 +0300

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите день недели (1-7)");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x==1) 
+{
+    System.Console.WriteLine("Понедельник");
+}
+else if (x==2)
+{
+     Console.WriteLine("Вторник");
+}
+else if (x==3) 
+{
+Console.WriteLine("Среда");
+}
+else if (x ==4) 
+{
+Console.WriteLine("Черверг");
+}
+else if (x ==5) 
+{
+Console.WriteLine("Пятница");
+}
+else if (x == 6) 
+{
+Console.WriteLine("Суббота");
+}
+else if (x == 7) 
+{
+Console.WriteLine("Воскресенье");
+}
