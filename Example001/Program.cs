b3fa6836f4c0e8e0da9deafecdea46a3d4b7d327
artem.chernomazov <pechenkazz@mail.ru>
2022-08-22 22:49:48 +0300

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿//на вход принимаем число, а на выход получаем его квадрат
+
+/*Первый вариант
+int x;
+Console.WriteLine("Введите число: ");
+x = Convert.ToInt32(Console.ReadLine());
+//x = int.Parse(Console.ReadLine()); = то же самое что и предыдущая запись
+int y = x*x;
+Console.WriteLine("Result " + (x*x));
+Console.Write("result is ");
+Console.WriteLine(y);*/
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Result " + MathF.Pow(x,2));
+
